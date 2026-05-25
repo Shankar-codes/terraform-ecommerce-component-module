22636b1a7008d4a00ecbb3f3336c5aaf70a0bcc9
Shankar-codes <harikainteriors@gmail.com>
2026-05-25 18:26:05 +0530

    ecommerce component module

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+  default = "ellamma-roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "component" {
+}
+
+variable "rule_priority" {
+}
+
+variable "domain_name" {
+    default = "ellamma.fun"
+}

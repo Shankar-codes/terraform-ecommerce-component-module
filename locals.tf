332a3f97a1785719f96aad39de1939401e0aa1f2
Shankar-codes <harikainteriors@gmail.com>
2026-05-26 17:52:29 +0530

    ecommerce component module

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
   health_check_path = var.component == "frontend" ? "/" : "/health"
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-  listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn.value : local.backend_alb_listener_arn.value
+  listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
   host_context =var.component == "frontend" ? "${var.project_name}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {

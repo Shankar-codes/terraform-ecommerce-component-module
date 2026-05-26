8dc8e7c3a095388f8682a2e941827a77fb88f54b
Shankar-codes <harikainteriors@gmail.com>
2026-05-26 17:43:27 +0530

    ecommerce component module

--- main.tf
+++ main.tf
@@ -2,9 +2,8 @@
 resource "aws_instance" "main" {
   ami = local.ami_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = local.sg_id
+  vpc_security_group_ids = [local.sg_id]
   subnet_id = local.private_subnet_ids
-  vpc_id = local.vpc_id
   tags =merge(local.common_tags, {
       Name = "${var.project_name}-${var.environment}-${var.component}"
       Terraform = "true"

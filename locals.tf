d87949d47282c13a00d185b5ad6b8565f8a9fbb0
Shankar-codes <harikainteriors@gmail.com>
2026-05-26 20:22:29 +0530

    ecommerce component module

--- locals.tf
+++ locals.tf
@@ -3,12 +3,12 @@ locals {
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   ami_id = data.aws_ami.ellamma_roboshop.id
   sg_id = data.aws_ssm_parameter.sg_id.value
-  tg_port = var.component == "frontend" ? 80 : 8080
-  health_check_path = var.component == "frontend" ? "/" : "/health"
+  tg_port = "${var.component}" == "frontend" ? 80 : 8080
+  health_check_path = "${var.component}" == "frontend" ? "/" : "/health"
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-  listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
-  host_context =var.component == "frontend" ? "${var.project_name}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+  listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
+  host_context = "${var.component}" == "frontend" ? "${var.project_name}-${var.environment}.${var.domain_name}" : "${var.component}-backend-alb-${var.environment}.${var.domain_name}"
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {
     Project = var.project_name
